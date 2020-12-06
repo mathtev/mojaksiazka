4985be763aee584a7e3c32b79c4561917867f551
MATEUSZ RYCHLIK <mathtev@gmail.com>
2020-12-06 20:44:16 +0100

    remove config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'mhewwfffxkbgvd';
-const PASSWORD = '1c5b245759a431a46ed5459503775d764bc540ccdf976a1f3ffabcb59bbcfab7';
-const HOST = 'ec2-52-208-175-161.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd1goqn9rfrebmi';
